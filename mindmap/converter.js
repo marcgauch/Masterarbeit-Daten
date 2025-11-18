ee0fc99c610918ac827d9c119c3b7bfd0c169a9d
marcgauch <34353267+marcgauch@users.noreply.github.com>
2025-11-18 18:02:30 +0100

    CHANGED: folderstructure and layout

--- mindmap/converter.js
+++ mindmap/converter.js
@@ -1,6 +1,5 @@
 const fs = require("node:fs/promises");
 const { execSync } = require("node:child_process");
-const { send } = require("node:process");
 
 const PLANTUML_JAR = "../util/plantuml-mit-1.2025.10.jar";
 
@@ -8,7 +7,7 @@ const FILE_HEADER = `
 @startmindmap
 <style>
 mindmapDiagram{
-	.blue {
+	.root {
 		BackgroundColor LightBlue
 	}
 }
@@ -31,14 +30,14 @@ function treeToMarkdown(
   let prefix = "\t".repeat(indent + 1) + "* ";
 
   if (nameOfRoot) {
-    lines.push("\t".repeat(indent) + "* " + nameOfRoot + " <<blue>>");
+    lines.push("\t".repeat(indent) + "* " + nameOfRoot + " <<root>>");
     indent += 1;
   } else {
     prefix = "\t".repeat(indent) + "* ";
   }
   if (depth > 0) {
     for (const [index, [key, value]] of Object.entries(obj).entries()) {
-      lines.push(prefix + key + (indent === 0 ? " <<blue>>" : ""));
+      lines.push(prefix + key + (indent === 0 ? " <<root>>" : ""));
 
       if (value && typeof value === "object" && Object.keys(value).length > 0) {
         lines.push(
@@ -77,8 +76,8 @@ const createAndWriteFile = async (filename, tree, settings) => {
   }
 };
 
-function generateImageWithPlantuml(inputFile) {
-  const cmd = `java -jar "${PLANTUML_JAR}" -tpng -o generated "${inputFile}" -DPLANTUML_LIMIT_SIZE=24384`;
+function generateImageWithPlantuml(inputFile, savePath = "") {
+  const cmd = `java -jar "${PLANTUML_JAR}" -tpng -o generated${savePath} "${inputFile}" -DPLANTUML_LIMIT_SIZE=24384`;
   execSync(cmd, (error, stdout, stderr) => {
     if (error) {
       console.error(
@@ -111,18 +110,33 @@ const create = async (filename, tree, settings = {}) => {
     console.error("Filename and tree are required.");
     return;
   }
-  settings.depth = settings.depth || 999;
+  settings.depth = settings.depth ?? 999;
   settings.nameOfRoot = settings.nameOfRoot || null;
-  settings.sendLeftAfter = settings.sendLeftAfter || 999;
+  settings.sendLeftAfter = settings.sendLeftAfter ?? 999;
 
   await createAndWriteFile(`${filename}.plantuml`, tree, settings);
-  generateImageWithPlantuml(`${filename}.plantuml`);
+  generateImageWithPlantuml(`${filename}.plantuml`, settings.savePath);
   await deleteOldFile(`${filename}.plantuml`);
 };
 
+const deleteOldOutputDir = async () => {
+  try {
+    await fs.rmdir("generated", { recursive: true });
+    console.log("Old generated directory deleted successfully.");
+  } catch (err) {
+    if (err.code !== "ENOENT") {
+      console.error("Error deleting generated directory:", err);
+    }
+  }
+};
+
 const run = async () => {
+  await deleteOldOutputDir();
   const tree = await loadTree("mindmap.json");
-  create("mindmap", tree, { sendLeftAfter: 1 });
+  create("mindmap", tree["Einschränkungen"], {
+    nameOfRoot: "Einschränkungen",
+    sendLeftAfter: 0,
+  });
 
   create(
     "mindmap-only-physical",
@@ -138,5 +152,55 @@ const run = async () => {
     depth: 1,
     nameOfRoot: "Einschränkungen",
   });
+
+  // create mindmaps for every main category in Körperliche Einschränkungen
+  for (const [key, value] of Object.entries(
+    tree["Einschränkungen"]["Körperliche Einschränkungen"]
+  )) {
+    const keyCleaned = key
+      .toLowerCase()
+      .replace(/ /g, "-")
+      .replace(/ä/g, "ae")
+      .replace(/ö/g, "oe")
+      .replace(/ü/g, "ue")
+      .replace(/ß/g, "ss");
+    create(`mindmap-physical-${keyCleaned}`, value, {
+      nameOfRoot: key,
+      sendLeftAfter: Math.floor(Object.keys(value).length / 2) - 1,
+      savePath: "/physical-subcategories",
+    });
+    console.log(`
+\\begin{figure}[H]
+  \\caption{Mindmap der Elemente der Unterkategorie \\enquote{${key}} der körperlichen Einschränkungen}
+  \\includegraphics[width=\\textwidth]{content/00_assets/mindmaps/physical-subcategories/mindmap-physical-${keyCleaned}.png}
+  \\note{Eigene Abbildung}
+  \\label{fig:mindmap-physical-${keyCleaned}}
+\\end{figure}`);
+  }
+
+  // create mindmaps for every category in physical > Sinne
+  for (const [key, value] of Object.entries(
+    tree["Einschränkungen"]["Körperliche Einschränkungen"]["Sinne"]
+  )) {
+    const keyCleaned = key
+      .toLowerCase()
+      .replace(/ /g, "-")
+      .replace(/ä/g, "ae")
+      .replace(/ö/g, "oe")
+      .replace(/ü/g, "ue")
+      .replace(/ß/g, "ss");
+    create(`mindmap-physical-sinne-${keyCleaned}`, value, {
+      nameOfRoot: key,
+      sendLeftAfter: Math.floor(Object.keys(value).length / 2) - 1,
+      savePath: "/physical-subcategories/sinne",
+    });
+    console.log(`
+\\begin{figure}[H]
+  \\caption{Mindmap der Elemente der Unterkategorie \\enquote{${key}} der körperlichen Einschränkungen im Bereich Sinne}
+  \\includegraphics[width=\\textwidth]{content/00_assets/mindmaps/physical-subcategories/sinne/mindmap-physical-sinne-${keyCleaned}.png}
+  \\note{Eigene Abbildung}
+  \\label{fig:mindmap-physical-sinne-${keyCleaned}}
+\\end{figure}`);
+  }
 };
 run();
